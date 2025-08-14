3f52f90d3ea35fda94b77deadc5ed1670377e030
Kaev <demonhunter@hotmail.de>
2025-08-14 22:45:47 +0200

    added redirect to router

--- src/router.js
+++ src/router.js
@@ -7,9 +7,20 @@ const routes = [
   { path: '/wheel/:id', component: WheelRoom, props: true }
 ]
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+// Handle GitHub Pages SPA fallback with ?redirect= param
+router.beforeEach((to, from, next) => {
+  const redirect = to.query.redirect
+  if (redirect) {
+    next(redirect)
+  } else {
+    next()
+  }
 })
 
 export default router
